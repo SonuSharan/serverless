c75dad5d801cbc30e1acd443e2d0da5612409f0e
SonuSharan <86845115+SonuSharan@users.noreply.github.com>
2025-07-20 23:20:27 +0530

    first commit

---
+++ src/helper/cloud.ts
@@ -0,0 +1,39 @@
+import {
+  GetObjectCommand,
+  NoSuchKey,
+  S3Client,
+  S3ServiceException,
+} from "@aws-sdk/client-s3";
+
+/**
+ * Get a single object from a specified S3 bucket.
+ * @param {{ bucketName: string, key: string }}
+ */
+export const main = async ({ bucketName, key }) => {
+  const client = new S3Client({});
+
+  try {
+    const response = await client.send(
+      new GetObjectCommand({
+        Bucket: bucketName,
+        Key: key,
+      }),
+    );
+    // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+    const str = await response.Body.transformToString();
+    console.log(str);
+  } catch (caught) {
+    if (caught instanceof NoSuchKey) {
+      console.error(
+        `Error from S3 while getting object "${key}" from "${bucketName}". No such key exists.`,
+      );
+    } else if (caught instanceof S3ServiceException) {
+      console.error(
+        `Error from S3 while getting object from ${bucketName}.  ${caught.name}: ${caught.message}`,
+      );
+    } else {
+      throw caught;
+    }
+  }
+};
+
---
+++ src/lambda/serverless.ts
@@ -0,0 +1,8 @@
+
+const handler = async (event: any) => {
+    return {
+        status: "Success",
+        code: 200,
+        payload: "Jenkins pipeline is Successful."
+    }
+}
